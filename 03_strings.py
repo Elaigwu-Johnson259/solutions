c422c2fd4b0920cbbb582cbcd9e1136b02086a27
Johnson <johnpet259@gmail.com>
2026-09-10 15:38:52 +0100

    I have solved 03_strings

---
+++ 03_strings.py
@@ -0,0 +1,26 @@
+#Date: 10-9-2026
+#Exercise_1
+first = "john"
+last = "doe"
+print(f"Hello, {first.title()} {last.title()}!")
+
+
+#Exercise_2
+msg = "   I am sad today.   "
+msg = msg.strip()
+msg = msg.replace("sad", "happy")
+print(msg)
+
+
+#Exercise_3
+word = "Antigravity"
+print(word[0])
+print(word[-1])
+print(word[::-1])
+
+
+
+#Exercise_4
+string_repitition = "Go! " * 5
+print(string_repitition)
+
