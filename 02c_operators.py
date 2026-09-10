e3cd77a910eb0cb1ac410bafe951426605e101d5
johnson <johnpet259@gmail.com>
2026-09-10 14:45:30 +0100

    I have solved 02c_operators.py

---
+++ 02c_operators.py
@@ -0,0 +1,18 @@
+#Date: 10-09-2026
+
+#Exercise_1
+a = 10
+b = 20
+average = (a + b) / 2
+print(average)
+
+
+#Exercise_2
+result = 7 == 7
+print(result)
+
+
+#Exercise_3
+age = 19
+is_student = True
+print(age >= 18 and is_student)
