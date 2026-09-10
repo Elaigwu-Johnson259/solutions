789b03c615aea3f7648b40153d5423b3b316e6ff
Your Name <johnpet259@gmail.com>
2026-09-10 13:18:56 +0100

    i solved 01_hello_python

---
+++ 01_hello_python.py
@@ -0,0 +1,28 @@
+#10-9-26
+
+# Exercise 01: Hello, Python!
+# Run this script with: python exercises.py
+
+# TODO: Exercise 1
+# Write a print statement that outputs: "I am learning Python!"
+print("I am learning python!")
+
+# TODO: Exercise 2
+# Write a print statement that uses single quotes to output: 'Python is fun.'
+print("python is fun")
+
+# TODO: Exercise 3
+# Write a mathematical expression inside print() that calculates 2 raised to the power of 10.
+print(2**10)
+
+# TODO: Exercise 4
+# Write a multi-line comment below explaining what a print function does.
+"""
+this
+is 
+a 
+
+multi
+line
+comment
+"""
