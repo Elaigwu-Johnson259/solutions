3b4f9c646e0a88bccc00d3550d8f5160d826f977
Elaigwu <johnpet259@gmail.com>
2026-09-10 16:49:25 +0100

    lists_tuples

---
+++ 04_lists_tuples.py
@@ -0,0 +1,31 @@
+#Date: 10-9-2026
+#Exercise_1
+colors = ["red", "green", "blue"]
+colors.append("yellow")
+colors.insert(1, "orange")
+print(colors)
+
+
+
+#Exercise_2
+numbers = [40, 5, 14, 98, 2]
+numbers.sort()
+numbers.reverse()
+print(numbers)
+
+
+#Exercise_3
+point = ("x=10", "y=20", "z=30")
+px, py, pz = point
+print(px)
+print(py)
+print(pz)
+
+
+#Exercise_4
+grades = [88, 72, 95, 60]
+print(sorted(grades))
+print(grades)
+grades.sort()
+print(grades)
+
