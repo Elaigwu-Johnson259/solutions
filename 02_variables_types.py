f59dcc8c22eb795e034e9ebc2f9b03c50eae121e
johnson <johnpet259@gmail.com>
2026-09-10 14:11:54 +0100

    I solved 02_variables_types

---
+++ 02_variables_types.py
@@ -0,0 +1,20 @@
+# Date:  10-09-2026
+
+#Exercise 1
+score = 95
+print(score)
+
+#Exercise_2
+gpa = 3.8
+print(type(gpa))
+
+
+
+#Exercise_3
+is_weekend = True
+print(is_weekend)
+
+#Exercise_4
+price_str = "49.99"
+price_str = float(price_str)
+print(price_str)
