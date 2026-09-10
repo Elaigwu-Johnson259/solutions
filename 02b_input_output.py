aa53b49fff66b21928c3f353fd6f549b27cc413b
johnson <johnpet259@gmail.com>
2026-09-10 14:27:05 +0100

    I have solved 02b_input_output

---
+++ 02b_input_output.py
@@ -0,0 +1,20 @@
+#Date: 10-09-2026
+
+#Exercise_1
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+
+#Exercise_2
+age = input("Input your age: ")
+age = int(age)
+age = age + 1 
+print(f"You will be {age} next Year.")
+
+
+#Exercise_3
+city = "London"
+temperature = 18 
+print(f"The Temperature in {city} is {temperature}")
+
+#I REALLY APPPRECIATE SIR
