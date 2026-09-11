c85e29d619887d99fd10e103833952f65cb0ef2c
johnson <johnpet259@gmail.com>
2026-09-11 12:21:34 +0100

    I am through with 07_loops

---
+++ 07_loops.py
@@ -0,0 +1,37 @@
+#Date: 11-9-2026
+#Exercise_1
+total = 0
+numbers = range(51)
+for num in numbers:
+    total = total + num 
+print(total)
+
+
+
+# Exercise_2
+count = 1
+while count <= 15:
+    if count % 2 == 0:
+        count += 1
+        continue
+    else:
+        print(count)
+    count += 1
+
+
+
+#Exercise_3
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+for name in names:
+    if name == "Charlie":
+        print(name)
+        break
+
+
+# Exercise_4
+list_com = [len(name) for name in names]
+print(list_com)
+
+# Use a list comprehension to create a new list containing the lengths of each word
+# in the 'names' list. Print the resulting list.
+
